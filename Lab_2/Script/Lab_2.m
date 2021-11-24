57ed1472ea4e1f61a8f72b2e302b8e12b9e5f179
DianaKhayretdinova <55025029+DianaKhayretdinova@users.noreply.github.com>
2021-11-25 02:16:15 +0300

    к

--- Lab_2/Script/Lab_2.m
+++ Lab_2/Script/Lab_2.m
@@ -10,8 +10,8 @@ n = readmatrix('data1.csv'); %показания измерителя дефор
 delta_n = n(2:end,:) - n(1:end-1,:);
 
 output_data(:,1) = 50:50:500;                           %нагрузка
-output_data(:,2) = round(mean(delta_n(:,1:2),2),0);    %продольная деформация, деления
-output_data(:,3) = round(mean(delta_n(:,3:4),2),0);    %поперечная деформация, деления
+output_data(:,2) = mean(delta_n(:,1:2),2);    %продольная деформация, деления
+output_data(:,3) = mean(delta_n(:,3:4),2);    %поперечная деформация, деления
 output_data(:,4) = output_data(:,2)*K*10^6;             % продольная деформация
 output_data(:,5) = output_data(:,3)*K*10^6;             % поперечная деформация
 output_data(:,6) = output_data(:,1)/S;                  % напряжение
@@ -19,11 +19,11 @@ output_data(:,7) = - output_data(:,5)./output_data(:,4);          %коэфф П
 output_data(:,8) = output_data(1,6)./output_data(:,4)*10^-3*10^6; %модуль Юнга
 
 writematrix(round(output_data,2), 'output_1.csv');
-    x =  (mean(n(1,1:2),2) - mean(n(2:end,1:2),2))*K *10^4 ; % продольная деформация с увеличением нагрузки
+    x =  (mean(n(1,1:2),2) - mean(n(2:end,1:2),2))*K *10^6 ; % продольная деформация с увеличением нагрузки
     y =  output_data(:,6);                            %напряжение с увеличением нагрузки
     p = polyfit(x,y,1);
     plot(x,y,'o',x, polyval(p,x));
-    xlabel('\epsilon_{xx}, *10^{-4}');
+    xlabel('\epsilon_{xx}, *10^{-6}');
     ylabel('\sigma, МПа');
     grid on 
  E = mean(output_data(1:8,8)); % убрали 9 и 10 значения
