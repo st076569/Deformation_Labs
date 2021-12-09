e27f75fc2edb67bfcb2c277a82dbb66a0158145d
st076569 <st076569@student.spbu.ru>
2021-12-09 03:52:55 +0300

    new

--- Lab_5/Script/delta_x.m
+++
@@ -1,8 +0,0 @@
-function [d_a] = delta_x(a,t_an) %cлучайная погрешность
-n = length(a);
-a_m = mean(a);
-d_i_2 = (a - a_m).^2;
-s_x = sqrt(sum(d_i_2,'all')/(n*(n-1))); %средняя погрешность среднего
-d_a = t_an*s_x;
-end
-
